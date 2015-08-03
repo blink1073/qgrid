289fd03ca864a213f9f42cded1be4072faa1b94b
Steven Silvester <steven.silvester@ieee.org>
2015-08-02 20:26:46 -0500

    Add the ipython widget

--- qgrid/__init__.py
+++ qgrid/__init__.py
@@ -1,25 +1,46 @@
-from .grid import SlickGrid
+from .grid import SlickGrid, QGridWidget
+import os
+
+QGRIDJS_PATH = os.path.join(
+        os.path.dirname(__file__),
+        'qgridjs',
+    )
 
 
 def show_grid(data_frame, remote_js=False):
     return SlickGrid(data_frame, remote_js)
 
 
+def edit_grid(data_frame, remote_js=False):
+    # Lazy imports so we don't pollute the namespace.
+    from IPython.html.widgets import Button, HBox
+    from IPython.display import display, Javascript
+
+    # create a visualization for the dataframe
+    grid = QGridWidget(df=data_frame)
+
+    add_row = Button(description="Add Row")
+    add_row.on_click(grid.add_row)
+
+    rem_row = Button(description="Remove Row")
+    rem_row.on_click(grid.remove_row)
+
+    with open(os.path.join(QGRIDJS_PATH, 'qgrid.widget.js')) as fid:
+        display(Javascript(fid.read()))
+    display(HBox((add_row, rem_row)), grid)
+
+    return grid
+
+
 def nbinstall(user=True, overwrite=False):
     """
     """
     # Lazy imports so we don't pollute the namespace.
-    import os
     from IPython.html.nbextensions import install_nbextension
     from IPython import version_info
 
-    qgridjs_path = os.path.join(
-        os.path.dirname(__file__),
-        'qgridjs',
-    )
-
     install_nbextension(
-        qgridjs_path,
+        QGRIDJS_PATH,
         overwrite=overwrite,
         symlink=False,
         verbose=0,
